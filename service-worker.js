f3af7ef4fe2388a9513600527e0e832897c8df83
Jens-Horstmann <Jens_Horstmann@gmx.net>
2024-11-16 01:15:34 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('my-cache').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/styles.css',
+          '/images/icon-192x192.png',
+          '/images/icon-512x512.png',
+          '/styles.css'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
