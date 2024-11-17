8f9eba66230c1b4c56152d92da35f7a123f129e8
Jens-Horstmann <Jens_Horstmann@gmx.net>
2024-11-17 21:31:58 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,22 @@
+const CACHE_NAME = 'pwa-cache-v5';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon-192x192.png',
+    '/icon-512x512.png',
+    '/styles.css'
+];
+
 self.addEventListener('install', event => {
     event.waitUntil(
-      caches.open('my-cache').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/icon-192x192.png',
-          '/icon-512x512.png',
-          '/styles.css'
-        ]);
-      })
+      caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
   });
   
   self.addEventListener('fetch', event => {
     event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
     );
   });
-  
